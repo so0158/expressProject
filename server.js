7e7352106eea4a6df111f896177971abb852e282
ASTEMS\Astems Design Team <sunshine.lee.sl@gmail.com>
2023-06-15 15:00:02 +0900

    at work2

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/blog.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.set('port', process.env.PORT || 8080)
 app.use('css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/public/js')))
-app.use('/cssasdasd', express.static(path.join(__dirname, '/public/cssasdasd')))
-app.use('/img', express.static(path.join(__dirname, '/public/img')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/js')))
+//app.use('/cssasdasd', express.static(path.join(__dirname, '/public/cssasdasd')))
+//app.use('/img', express.static(path.join(__dirname, '/public/img')))
 
 app.listen(8080, () => {
     console.log('성공~')
